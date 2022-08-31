bfed82b1d26a8a2fad7f06e029a8098dd1862577
Lourdes-Code <lourdes.code1990@gmail.com>
2022-08-31 18:26:30 -0400

    Actualizando apuntes de JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+let verbo = "programar";
+let mensaje = "Estoy aprendiendo a " + verbo + " con FreeCodeCamp";
+console.log(mensaje); 
+
+let nombreCompleto = "Berny " + "Poblete";
+console.log(nombreCompleto);
